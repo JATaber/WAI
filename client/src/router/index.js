822f75dcaa9facbd505ed0b17721053285efd485
James Taber <JATaber@fullsail.edu>
2018-04-04 01:29:59 -0500

    working on connecting Vue and Express

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/posts',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
