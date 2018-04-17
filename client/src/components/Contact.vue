3c7663438cc7380612451dfa23b7d795c8e51f88
James Taber <JATaber@fullsail.edu>
2018-04-17 10:58:02 -0500

    working on modal

--- client/src/components/Contact.vue
+++ client/src/components/Contact.vue
@@ -3,19 +3,19 @@
     <img class="banner" src="../assets/aki_contact.png" alt="contact">
 
     <b-container fluid>
-      <b-row>
-        <b-col class="jumbotron" md="5">
-          <icon name="brands/twitch"></icon><p>twitch.tv/awkwardkittyinc</p><br>
-          <icon name="brands/youtube"></icon><p>youtube.com/awkwardkittyinc</p><br>
-          <icon name="brands/twitter"></icon><p>twitter.com/AwkwardKittyInc</p><br>
-          <icon name="brands/instagram"></icon><p>instagram.com/awkwardkittyinc</p><br>
-        </b-col>
+      <b-card-group columns>
+        <b-card class="jumbotron">
+          <a href="http://twitch.tv/awkwardkittyinc"><icon name="brands/twitch"></icon> twitch.tv/awkwardkittyinc</a><br>
+          <a href="http://youtube.com/awkwardkittying"><icon name="brands/youtube"></icon> youtube.com/awkwardkittyinc</a><br>
+          <a href="http://twitter.com/AwkwardKittyInc"><icon name="brands/twitter"></icon> twitter.com/AwkwardKittyInc</a><br>
+          <a href="http://instagram.com/awkwardkittyinc"><icon name="brands/instagram"></icon> instagram.com/awkwardkittyinc</a><br>
+        </b-card>
+        <b-card class="ml-auto jumbotron">
 
-        <b-col md="6" class="ml-auto jumbotron">
-
-          <b-form @submit="showModal=true" @reset="onReset" v-if="show">
+          <b-form @submit="modal" @reset="onReset" v-if="show">
             <b-form-group id="email"
-                          label="Email address:"
+                          horizontal
+                          label="Email:"
                           lable-for="emailInput"
                           description="We'll never share your email with anyone else.">
               <b-form-input id="emailInput"
@@ -25,8 +25,10 @@
                             placeholder="Enter email">
               </b-form-input>
             </b-form-group>
-            <b-form-group id="group2"
-                          label="Your Name:"
+            <b-form-group id="name"
+                          horizontal
+                          label="Name:"
+                          description="What should I call you?"
                           label-for="nameInput">
               <b-form-input id="nameInput"
                             type="text"
@@ -39,14 +41,20 @@
                              v-model="form.comment"
                              placeholder="Please type your question or comment here."
                              :rows="3"
-                             :max-rows="6">
+                             :max-rows="12">
             </b-form-textarea>
-            <b-button type="submit" variant="primary">Submit</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
+            <b-btn v-b-modal.formSub type="submit" variant="primary">Submit</b-btn>
+            <b-button type="reset" variant="primary">Reset</b-button>
           </b-form>
-        </b-col>
-      </b-row>
+        </b-card>
+      </b-card-group>
     </b-container>
+    <b-modal id="formSub" ref="formSub" hide-footer title="Thank You!">
+      <div class="d-block text-center">
+        <h3>We will get back to you as soon as we can!</h3>
+      </div>
+      <b-btn class="mt-3" variant="primary" block @click="hide">OK</b-btn>
+    </b-modal>
   </div>
 </template>
 
@@ -60,8 +68,7 @@ export default {
         name: '',
         comment: ''
       },
-      show: true,
-      showModal: false
+      show: true
     }
   },
   methods: {
@@ -71,6 +78,12 @@ export default {
       this.form.comment = ''
       this.show = false
       this.$nextTick(() => { this.show = true })
+    },
+    modal () {
+      this.$refs.formSub.show()
+    },
+    hide () {
+      this.$refs.formSub.hide()
     }
   }
 }
